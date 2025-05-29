24c6f22f1872f4ae7aabf432380b19aaa9760826
LuanODias <luoliveidias@gmail.com>
2025-05-29 18:35:38 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,30 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open={true}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicionar Clinica</DialogTitle>
+            <DialogDescription>
+              Adicione uma clinica para que você possa gerenciar seus pacientes
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+      )
+    </div>
+  );
+};
+
+export default ClinicFormPage;
