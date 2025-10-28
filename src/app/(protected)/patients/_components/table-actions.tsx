5ffdd600c76ce5212feec24fab96c7604e6e1a16
LuanODias <luoliveidias@gmail.com>
2025-10-28 18:44:14 -0300

    feat: ensure patients can be deleted

--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -12,6 +12,19 @@ import { Dialog } from "@/components/ui/dialog";
 import UpsertPatientForm from "./upsert-patient-form";
 import { patientsTable } from "@/db/schema";
 import { useState } from "react";
+import { useAction } from "next-safe-action/hooks";
+import { toast } from "sonner";
+import { deletePatient } from "@/actions/upsert-patient/delete-patient";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 interface PatientsTableActionsProps {
   patient: typeof patientsTable.$inferSelect;
@@ -19,33 +32,78 @@ interface PatientsTableActionsProps {
 
 const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
   const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+  const [deleteDialogIsOpen, setDeleteDialogIsOpen] = useState(false);
+
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso");
+      setDeleteDialogIsOpen(false);
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente");
+    },
+  });
+
+  const handleDeletePatientClick = () => {
+    deletePatientAction.execute({ id: patient.id });
+  };
+
   return (
-    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button variant="ghost" size="icon">
-            <MoreVerticalIcon className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
-            <EditIcon />
-            Editar
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <TrashIcon />
-            Excluir
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <UpsertPatientForm
-        isOpen={upsertDialogIsOpen}
-        patient={patient}
-        onSuccess={() => setUpsertDialogIsOpen(false)}
-      />
-    </Dialog>
+    <>
+      <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+              <EditIcon />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault();
+                setDeleteDialogIsOpen(true);
+              }}
+            >
+              <TrashIcon />
+              Excluir
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UpsertPatientForm
+          isOpen={upsertDialogIsOpen}
+          patient={patient}
+          onSuccess={() => setUpsertDialogIsOpen(false)}
+        />
+      </Dialog>
+      <AlertDialog
+        open={deleteDialogIsOpen}
+        onOpenChange={setDeleteDialogIsOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Tem certeza que deseja deletar esse paciente?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Essa ação não será revertida. Isso irá deletar o paciente e todas
+              as consultas agendadas.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDeletePatientClick}>
+              Deletar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
